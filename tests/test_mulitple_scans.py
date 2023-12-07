b2ae31c3ae3cc1aa3c18c4eba457bf5c48cfa689
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2023-12-07 16:46:15 +0100

    test consecutive scans

---
+++ tests/test_mulitple_scans.py
@@ -0,0 +1,191 @@
+import os
+import pickle
+from pathlib import PosixPath
+
+import asyncio
+from typing import Awaitable, Callable, Any, Coroutine, Optional
+import zmq.asyncio
+
+import aiohttp
+import pytest
+from pydantic_core import Url
+
+from dranspose.ingester import Ingester
+from dranspose.ingesters.streaming_contrast import (
+    StreamingContrastIngester,
+    StreamingContrastSettings,
+)
+from dranspose.ingesters.streaming_xspress3 import (
+    StreamingXspressIngester,
+    StreamingXspressSettings,
+)
+from dranspose.protocol import (
+    EnsembleState,
+    StreamName,
+    WorkerName,
+    VirtualWorker,
+    VirtualConstraint,
+)
+from dranspose.worker import Worker, WorkerSettings
+
+
+@pytest.mark.asyncio
+async def test_multiple_scans(
+    controller: None,
+    reducer: Callable[[Optional[str]], Awaitable[None]],
+    create_worker: Callable[[Worker], Awaitable[Worker]],
+    create_ingester: Callable[[Ingester], Awaitable[Ingester]],
+    stream_pkls: Callable[
+        [zmq.Context[Any], int, os.PathLike[Any] | str, float, int],
+        Coroutine[Any, Any, None],
+    ],
+) -> None:
+    await reducer("examples.dummy.reducer:FluorescenceReducer")
+    await create_worker(
+        Worker(
+            settings=WorkerSettings(
+                worker_name=WorkerName("w1"),
+                worker_class="examples.dummy.worker:FluorescenceWorker",
+            ),
+        )
+    )
+    await create_ingester(
+        StreamingContrastIngester(
+            name=StreamName("contrast"),
+            settings=StreamingContrastSettings(
+                upstream_url=Url("tcp://localhost:5556"),
+                ingester_url=Url("tcp://localhost:10000"),
+            ),
+        )
+    )
+    await create_ingester(
+        StreamingXspressIngester(
+            name=StreamName("xspress3"),
+            settings=StreamingXspressSettings(
+                upstream_url=Url("tcp://localhost:9999"),
+                ingester_url=Url("tcp://localhost:10001"),
+            ),
+        )
+    )
+
+    async with aiohttp.ClientSession() as session:
+        st = await session.get("http://localhost:5000/api/v1/config")
+        state = EnsembleState.model_validate(await st.json())
+        while {"contrast", "xspress3"} - set(state.get_streams()) != set():
+            await asyncio.sleep(0.3)
+            st = await session.get("http://localhost:5000/api/v1/config")
+            state = EnsembleState.model_validate(await st.json())
+
+        resp = await session.post(
+            "http://localhost:5000/api/v1/parameter/roi1",
+            json=[0, 10],
+        )
+        assert resp.status == 200
+        await resp.json()
+
+        ntrig = 20
+        mp = {
+            "contrast": [
+                [VirtualWorker(constraint=VirtualConstraint(i)).model_dump(mode="json")]
+                for i in range(ntrig)
+            ],
+            "xspress3": [
+                [VirtualWorker(constraint=VirtualConstraint(i)).model_dump(mode="json")]
+                for i in range(ntrig)
+            ],
+        }
+        resp = await session.post(
+            "http://localhost:5000/api/v1/mapping",
+            json=mp,
+        )
+        assert resp.status == 200
+        await resp.json()
+
+    context = zmq.asyncio.Context()
+
+    asyncio.create_task(
+        stream_pkls(
+            context,
+            9999,
+            PosixPath("tests/data/xspress3mini-dump20.pkls"),
+            0.001,
+            zmq.PUB,
+        )
+    )
+    asyncio.create_task(
+        stream_pkls(
+            context, 5556, PosixPath("tests/data/contrast-dump.pkls"), 0.001, zmq.PUB
+        )
+    )
+
+    async with aiohttp.ClientSession() as session:
+        st = await session.get("http://localhost:5000/api/v1/progress")
+        content = await st.json()
+        while not content["finished"]:
+            await asyncio.sleep(0.3)
+            st = await session.get("http://localhost:5000/api/v1/progress")
+            content = await st.json()
+
+        st = await session.get("http://localhost:5001/api/v1/result/pickle")
+        content = await st.content.read()
+        result = pickle.loads(content)
+        print("content", result)
+        assert len(result["map"].keys()) == ntrig
+
+    for _ in range(2):
+        await stream_pkls(
+            context,
+            9999,
+            PosixPath("tests/data/xspress3mini-dump20.pkls"),
+            0.001,
+            zmq.PUB,
+        )
+
+    # second scan
+    async with aiohttp.ClientSession() as session:
+        resp = await session.post(
+            "http://localhost:5000/api/v1/mapping",
+            json=mp,
+        )
+        assert resp.status == 200
+        await resp.json()
+
+    await stream_pkls(
+        context,
+        9999,
+        PosixPath("tests/data/xspress3mini-dump20.pkls"),
+        0.001,
+        zmq.PUB,
+        begin=30,
+    )
+
+    asyncio.create_task(
+        stream_pkls(
+            context,
+            9999,
+            PosixPath("tests/data/xspress3mini-dump20.pkls"),
+            0.001,
+            zmq.PUB,
+        )
+    )
+    asyncio.create_task(
+        stream_pkls(
+            context, 5556, PosixPath("tests/data/contrast-dump.pkls"), 0.001, zmq.PUB
+        )
+    )
+
+    async with aiohttp.ClientSession() as session:
+        st = await session.get("http://localhost:5000/api/v1/progress")
+        content = await st.json()
+        while not content["finished"]:
+            await asyncio.sleep(0.3)
+            st = await session.get("http://localhost:5000/api/v1/progress")
+            content = await st.json()
+
+        st = await session.get("http://localhost:5001/api/v1/result/pickle")
+        content = await st.content.read()
+        result = pickle.loads(content)
+        print("content", result)
+        assert len(result["map"].keys()) == ntrig
+
+    context.destroy()
