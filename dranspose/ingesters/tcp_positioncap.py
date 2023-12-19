6fc6500b4ec966d62255b799e8f5b4a55868016d
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2023-12-19 13:47:23 +0100

    pcap ingester

---
+++ dranspose/ingesters/tcp_positioncap.py
@@ -0,0 +1,74 @@
+import asyncio
+from asyncio import StreamWriter
+from typing import AsyncGenerator, Optional
+
+import zmq
+from pydantic_core import Url
+
+from dranspose.event import StreamData
+from dranspose.ingester import Ingester, IngesterSettings
+from dranspose.protocol import StreamName
+
+
+class TcpPcapSettings(IngesterSettings):
+    upstream_url: Url
+
+
+class TcpPcapIngester(Ingester):
+    """
+    A simple ingester class to comsume a stream from the streaming-receiver repub port
+    """
+
+    def __init__(self, settings: Optional[TcpPcapSettings] = None) -> None:
+        if settings is not None:
+            self._pcap_settings = settings
+        else:
+            self._pcap_settings = TcpPcapSettings()
+
+        super().__init__(settings=self._pcap_settings)
+        self.writer: Optional[StreamWriter] = None
+
+    async def run_source(self, stream: StreamName) -> AsyncGenerator[StreamData, None]:
+        reader, self.writer = await asyncio.open_connection(
+            self._pcap_settings.upstream_url.host, self._pcap_settings.upstream_url.port
+        )
+        self.writer.write(b"\n")
+        data = await reader.readline()
+        assert data == b"OK\n"
+
+        self._logger.info("connected to %s", self._pcap_settings.upstream_url)
+
+        while True:
+            self._logger.debug("clear up insocket")
+            line = await reader.readline()
+            self._logger.debug("received line %s", line)
+            if not line.startswith(b"arm_time: "):
+                self._logger.debug("discard non-start packet")
+            else:
+                self._logger.info("start of new sequence %s", line)
+                header = [line]
+                while line := await reader.readline():
+                    header.append(line)
+                    if line.strip() == b"":
+                        break
+                self._logger.info("use header %s", header)
+                frame = zmq.Frame(b"".join(header))
+                yield StreamData(typ="PCAP", frames=[frame])
+                break
+        while True:
+            line = await reader.readline()
+            self._logger.debug("send frame %s", line)
+            frame = zmq.Frame(line)
+            yield StreamData(typ="PCAP", frames=[frame])
+            if line.startswith(b"END"):
+                break
+
+        while True:
+            self._logger.debug("discarding messages until next run")
+            await reader.readline()
+
+    async def stop_source(self, stream: StreamName) -> None:
+        if self.writer:
+            self.writer.close()
+            await self.writer.wait_closed()
+            self.writer = None
--- tests/test_panda.py
+++ tests/test_panda.py
@@ -1,8 +1,22 @@
 import asyncio
-import logging
-from typing import Callable, Coroutine
+from typing import Awaitable, Callable, Coroutine, Optional
+
+import aiohttp
 
 import pytest
+from pydantic_core import Url
+
+from dranspose.ingester import Ingester
+from dranspose.ingesters.tcp_positioncap import TcpPcapIngester, TcpPcapSettings
+from dranspose.protocol import (
+    EnsembleState,
+    StreamName,
+    WorkerName,
+    VirtualWorker,
+    VirtualConstraint,
+)
+
+from dranspose.worker import Worker
 
 
 @pytest.mark.asyncio
@@ -17,10 +31,66 @@ async def test_panda_pcap(
     data = await reader.readline()
     assert data == b"OK\n"
     while data := await reader.readline():
-        logging.info("got line %s", data)
         if data.startswith(b"END"):
             break
     writer.close()
     await writer.wait_closed()
 
     await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_pcapingester(
+    controller: None,
+    reducer: Callable[[Optional[str]], Awaitable[None]],
+    create_worker: Callable[[WorkerName], Awaitable[Worker]],
+    create_ingester: Callable[[Ingester], Awaitable[Ingester]],
+    stream_pcap: Callable[[int], Coroutine[None, None, None]],
+) -> None:
+    await reducer(None)
+    await create_worker(WorkerName("w1"))
+    await create_ingester(
+        TcpPcapIngester(
+            settings=TcpPcapSettings(
+                ingester_streams=[StreamName("pcap")],
+                upstream_url=Url("tcp://localhost:8889"),
+            ),
+        )
+    )
+
+    async with aiohttp.ClientSession() as session:
+        st = await session.get("http://localhost:5000/api/v1/config")
+        state = EnsembleState.model_validate(await st.json())
+        while {"pcap"} - set(state.get_streams()) != set():
+            await asyncio.sleep(0.3)
+            st = await session.get("http://localhost:5000/api/v1/config")
+            state = EnsembleState.model_validate(await st.json())
+
+        ntrig = 10
+        resp = await session.post(
+            "http://localhost:5000/api/v1/mapping",
+            json={
+                "pcap": [
+                    [
+                        VirtualWorker(constraint=VirtualConstraint(2 * i)).model_dump(
+                            mode="json"
+                        )
+                    ]
+                    for i in range(1, ntrig)
+                ],
+            },
+        )
+        assert resp.status == 200
+        await resp.json()
+
+    asyncio.create_task(stream_pcap(ntrig - 1))
+
+    async with aiohttp.ClientSession() as session:
+        st = await session.get("http://localhost:5000/api/v1/progress")
+        content = await st.json()
+        while not content["finished"]:
+            await asyncio.sleep(0.3)
+            st = await session.get("http://localhost:5000/api/v1/progress")
+            content = await st.json()
+
+    print(content)
