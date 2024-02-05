cd0c1ed484cb31a1fdeab29b20814ae419e9ed59
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-02-05 16:52:09 +0100

    more debug

--- examples/slow/reducer.py
+++ examples/slow/reducer.py
@@ -3,6 +3,8 @@ import time
 
 from dranspose.event import ResultData
 
+logger = logging.getLogger(__name__)
+
 
 class SlowReducer:
     def __init__(self, **kwargs):
@@ -10,10 +12,12 @@ class SlowReducer:
 
     def process_result(self, result: ResultData, parameters=None):
         print(result)
+        logger.info("processing_result %s", result)
         # if result.payload:
         time.sleep(0.5)
+        logger.info("slept for 500ms")
         self.publish["map"][result.event_number] = 1
-        logging.info("updated publish to %s", self.publish)
+        logger.info("updated publish to %s", self.publish)
 
     def finish(self, parameters=None):
         print("finished dummy reducer work")
