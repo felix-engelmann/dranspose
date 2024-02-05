96147a3e4f9668e449ba3b6b013fb602ca820e95
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-02-05 14:21:43 +0100

    added documentation for more protocols

---
+++ docs/reference/protocols/STINS.md
@@ -0,0 +1,1 @@
+:::dranspose.data.stream1
--- dranspose/data/stream1.py
+++ dranspose/data/stream1.py
@@ -8,6 +8,17 @@ class Stream1(BaseModel):
 
 
 class Stream1Start(Stream1):
+    """
+    Example:
+        ``` py
+        Stream1Start(
+            msg_number=374,
+            htype='header',
+            filename='/data/visitors/....test.h5'
+        )
+        ```
+    """
+
     model_config = ConfigDict(extra="allow")
 
     htype: Literal["header"]
@@ -15,6 +26,27 @@ class Stream1Start(Stream1):
 
 
 class Stream1Data(Stream1):
+    """
+    Example:
+        ``` py
+        Stream1Data(
+            msg_number=375,
+            htype='image',
+            frame=0,
+            shape=[831, 1475],
+            type='float32',
+            compression='none',
+            data=array([[0., 0., 0., ..., 0., 0., 0.],
+                [0., 0., 0., ..., 0., 0., 0.],
+                [0., 0., 0., ..., 0., 0., 0.],
+                ...,
+                [0., 0., 0., ..., 0., 0., 0.],
+                [0., 0., 0., ..., 0., 0., 0.],
+                [0., 0., 0., ..., 0., 0., 0.]], dtype=float32)
+        )
+        ```
+    """
+
     model_config = ConfigDict(extra="allow")
 
     htype: Literal["image"]
@@ -25,9 +57,22 @@ class Stream1Data(Stream1):
 
 
 class Stream1End(Stream1):
+    """
+    Example:
+        ``` py
+        Stream1End(
+            msg_number=376,
+            htype='series_end'
+        )
+        ```
+    """
+
     model_config = ConfigDict(extra="allow")
 
     htype: Literal["series_end"]
 
 
 Stream1Packet = TypeAdapter(Stream1Start | Stream1Data | Stream1End)
+"""
+A union type for STINS packets
+"""
