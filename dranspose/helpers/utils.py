e352911030d94f9a39075c3fd1b04e84b3404fbe
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-03-14 12:02:32 +0100

    ignore errors on waiting

--- dranspose/helpers/utils.py
+++ dranspose/helpers/utils.py
@@ -43,3 +43,7 @@ async def cancel_and_wait(task):
         await task
     except asyncio.CancelledError:
         pass
+    except StopIteration:
+        pass
+    except Exception as e:
+        logging.error("cancel and wait task raised %s", e.__repr__())
