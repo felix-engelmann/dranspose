90cf4be8570eddf1a2799627d26be35b8c5b9d9d
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-04-26 21:51:16 +0200

    workload tests

--- dranspose/workload_generator.py
+++ dranspose/workload_generator.py
@@ -1,8 +1,11 @@
+import asyncio
 import logging
+from asyncio import Task
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator, Any
 
-import zmq
+import numpy as np
+import zmq.asyncio
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -16,13 +19,30 @@ class SocketSpec(BaseModel):
 
 class WorkloadGenerator:
     def __init__(self, **kwargs: Any) -> None:
-        self.context = zmq.Context()
+        self.context = zmq.asyncio.Context()
         self.socket: zmq.Socket[Any] | None = None
+        self.task: Task[Any] | None = None
+
+    def status(self):
+        if self.task is None:
+            return True
+        return self.task.done()
 
     def open_socket(self, spec: SocketSpec) -> None:
         self.close_socket()
         self.socket = self.context.socket(spec.type)
         self.socket.bind(f"tcp://*:{spec.port}")
+
+    async def packets(self, num, time):
+        for i in range(num):
+            logger.debug("send packet %d", i)
+            data = np.zeros((10, 10))
+            await self.socket.send(data)
+            await asyncio.sleep(time)
+
+    def empty_packets(self, num, time):
+        logger.debug("create task")
+        self.task = asyncio.create_task(self.packets(num, time))
 
     def close_socket(self) -> None:
         if self.socket is not None:
@@ -50,7 +70,8 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/api/v1/status")
 async def get_status() -> bool:
-    return True
+    global gen
+    return gen.status()
 
 
 @app.post("/api/v1/open_socket")
@@ -59,6 +80,14 @@ async def sock(sockspec: SocketSpec) -> SocketSpec:
     print(sockspec)
     gen.open_socket(sockspec)
     return sockspec
+
+
+@app.post("/api/v1/frames")
+async def frames(number: int = 5, time: float = 0.2) -> bool:
+    global gen
+    logger.debug("start of packets")
+    gen.empty_packets(number, time)
+    return True
 
 
 @app.post("/api/v1/close_socket")
--- tests/test_workload.py
+++ tests/test_workload.py
@@ -1,5 +1,6 @@
+import asyncio
 import logging
-from typing import Awaitable, Callable, Optional
+from typing import Awaitable, Callable, Optional, Any
 
 import aiohttp
 
@@ -8,24 +9,62 @@ import zmq.asyncio
 import zmq
 
 
+async def consume(
+    ctx: zmq.Context[Any], num: int, typ: int = zmq.PULL, port: int = 9999
+) -> int:
+    s = ctx.socket(typ)
+    logging.info("created socket")
+    s.connect(f"tcp://127.0.0.1:{port}")
+    if typ == zmq.SUB:
+        s.setsockopt(zmq.SUBSCRIBE, b"")
+        num -= 1
+    logging.info("connected socket to port %s", port)
+    for _ in range(num):
+        data = await s.recv_multipart(copy=False)
+        logging.info("received data %s", data)
+
+    return num
+
+
+@pytest.mark.parametrize("srv,cli", [(zmq.PUSH, zmq.PULL), (zmq.PUB, zmq.SUB)])
 @pytest.mark.asyncio
 async def test_debugger(
-    workload_generator: Callable[[Optional[int]], Awaitable[None]],
+    workload_generator: Callable[[Optional[int]], Awaitable[None]], srv, cli
 ) -> None:
     await workload_generator(5003)
 
+    ctx = zmq.asyncio.Context()
     async with aiohttp.ClientSession() as session:
         st = await session.get("http://localhost:5003/api/v1/status")
         state = await st.json()
         logging.info("gen state %s", state)
 
+        task = asyncio.create_task(consume(ctx, 5, typ=cli))
+        logging.info("created consumer task")
+
         st = await session.post(
             "http://localhost:5003/api/v1/open_socket",
-            json={"type": zmq.PUB, "port": 4242},
+            json={"type": srv, "port": 9999},
         )
         state = await st.json()
         logging.info("open %s", state)
 
+        st = await session.post(
+            "http://localhost:5003/api/v1/frames",
+        )
+        state = await st.json()
+        logging.info("sending frames %s", state)
+
+        st = await session.get("http://localhost:5003/api/v1/status")
+        state = await st.json()
+        while not state:
+            await asyncio.sleep(0.3)
+            st = await session.get("http://localhost:5003/api/v1/status")
+            state = await st.json()
+
         st = await session.post("http://localhost:5003/api/v1/close_socket")
         state = await st.json()
         logging.info("close %s", state)
+
+    await task
+    ctx.destroy(linger=0)
