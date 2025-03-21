a808b8db5b4c0ddc347cbe3ea4227fc9657234fd
Michele Cascella <michele.cascella@maxiv.lu.se>
2025-03-21 15:46:33 +0100

    print some debugging

--- examples/parser/lecroy.py
+++ examples/parser/lecroy.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Any
 
-from dranspose.data.lecroy import LecroyData
+from dranspose.data.lecroy import LecroyParsed
 from dranspose.event import EventData
 from dranspose.middlewares.lecroy import parse
 from dranspose.protocol import StreamName
@@ -18,7 +18,14 @@ class LecroyWorker:
     ) -> None:
         if "lecroy" in event.streams:
             res = parse(event.streams[StreamName("lecroy")])
-            if isinstance(res, LecroyData):
-                logger.debug("parsed packet is %s", res)
+            if isinstance(res, LecroyParsed):
+                for i, ch in enumerate(res.channels):
+                    logger.info(f"looking at channel {ch}")
+                    logger.info(f"meta packet is {res.meta[i]}")
+                    logger.info(f"trace shape is {res.data[i].dtype}")
+                    logger.info(f"timestamps list len is {len(res.timestamps[i])}")
+                    logger.debug("traces %s", res.data[i])
+                    logger.info("timestamps list len is %s", len(res.timestamps[i]))
+
             else:
                 logger.info("control packet is %s", res)
