a380c8c0fc915c19e5c55fff3dc98510eb21fbbd
Michele Cascella <michele.cascella@maxiv.lu.se>
2025-03-26 15:40:44 +0100

    add more debug

--- examples/parser/lecroy.py
+++ examples/parser/lecroy.py
@@ -11,11 +11,12 @@ logger = logging.getLogger(__name__)
 
 class LecroyWorker:
     def __init__(self, *args: tuple[Any], **kwargs: dict[str, Any]) -> None:
-        pass
+        logger.info("LecroyWorker init")
 
     def process_event(
         self, event: EventData, *args: tuple[Any], **kwargs: dict[str, Any]
     ) -> None:
+        logger.debug(f"LecroyWorker {event=}")
         if "lecroy" in event.streams:
             res = parse(event.streams[StreamName("lecroy")])
             if isinstance(res, LecroyParsed):
@@ -24,8 +25,9 @@ class LecroyWorker:
                     logger.info(f"meta packet is {res.meta[i]}")
                     logger.info(f"trace shape is {res.data[i].dtype}")
                     logger.info(f"timestamps list len is {len(res.timestamps[i])}")
-                    logger.debug("traces %s", res.data[i])
-                    logger.info("timestamps list len is %s", len(res.timestamps[i]))
-
+                    logger.debug(
+                        f"traces {res.data[i]}",
+                    )
+                    logger.debug(f"timestamps list {res.timestamps[i]}")
             else:
                 logger.info("control packet is %s", res)
