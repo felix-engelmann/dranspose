46f3e52beb1b0f972e89636b84bb47f5f3d0504b
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2025-05-07 15:36:19 -0400

    fix replay test

--- tests/test_reducer.py
+++ tests/test_reducer.py
@@ -1,12 +1,13 @@
 import logging
 import os
 import pathlib
-import pickle
 from pathlib import PosixPath
 import h5pyd
 
 import asyncio
 from typing import Awaitable, Callable, Any, Coroutine, Optional
+
+import numpy as np
 import zmq.asyncio
 
 import aiohttp
@@ -136,54 +137,49 @@ async def test_reduction(
         "finished": True,
     }
 
-    async with aiohttp.ClientSession() as session:
-        st = await session.get("http://localhost:5001/api/v1/result/pickle")
-        content_bytes = await st.content.read()
-        result = pickle.loads(content_bytes)
-        assert result == {
-            "map": {
-                (-2.004051863, -2.002903037): {"roi1": 81},
-                (-2.001404022, -0.9992748245): {"roi1": 76},
-                (-2.000941193, 0.001238407776): {"roi1": 67},
-                (-2.000656853, 1.001692484): {"roi1": 49},
-                (-2.000562814, 2.0019763): {"roi1": 69},
-                (-0.6658021229, -2.002587864): {"roi1": 97},
-                (-0.6664982812, -0.9992643584): {"roi1": 75},
-                (-0.6666898134, 0.001223421204): {"roi1": 58},
-                (-0.6666922578, 1.001631271): {"roi1": 81},
-                (-0.6666616732, 2.001930725): {"roi1": 77},
-                (0.6676852429, -2.002412156): {"roi1": 91},
-                (0.6666162969, -0.9992055423): {"roi1": 81},
-                (0.6664269385, 0.001214749016): {"roi1": 76},
-                (0.6663550388, 1.00170782): {"roi1": 67},
-                (0.6663102034, 2.002018839): {"roi1": 49},
-                (2.001407847, -2.002363463): {"roi1": 69},
-                (2.000183463, -0.9992309104): {"roi1": 97},
-                (1.999963432, 0.001258902382): {"roi1": 75},
-                (1.999884878, 1.001690624): {"roi1": 58},
-                (1.999828501, 2.001959397): {"roi1": 81},
-            },
-            "version": {
-                "commit_hash": "1a474d1455f0d6c6113a22179b8fff1e98325916",
-                "branch_name": "main",
-                "timestamp": "2024-04-25T08:07:59+02:00",
-                "repository_url": "https://gitlab.maxiv.lu.se/DanMAX/pipeline/drp-tomo-buffer",
-            },
-        }
-        assert len(result["map"].keys()) == ntrig
-        assert (
-            result["version"]["commit_hash"]
-            == "1a474d1455f0d6c6113a22179b8fff1e98325916"
-        )
-
     def work() -> None:
         f = h5pyd.File("http://localhost:5001/", "r")
         logging.info("file %s", list(f.keys()))
         logging.info("map %s", list(f["map"].keys()))
         logging.info("version %s", f["version/commit_hash"][()])
+
+        should = {
+            (-2.004051863, -2.002903037): {"roi1": 81},
+            (-2.001404022, -0.9992748245): {"roi1": 76},
+            (-2.000941193, 0.001238407776): {"roi1": 67},
+            (-2.000656853, 1.001692484): {"roi1": 49},
+            (-2.000562814, 2.0019763): {"roi1": 69},
+            (-0.6658021229, -2.002587864): {"roi1": 97},
+            (-0.6664982812, -0.9992643584): {"roi1": 75},
+            (-0.6666898134, 0.001223421204): {"roi1": 58},
+            (-0.6666922578, 1.001631271): {"roi1": 81},
+            (-0.6666616732, 2.001930725): {"roi1": 77},
+            (0.6676852429, -2.002412156): {"roi1": 91},
+            (0.6666162969, -0.9992055423): {"roi1": 81},
+            (0.6664269385, 0.001214749016): {"roi1": 76},
+            (0.6663550388, 1.00170782): {"roi1": 67},
+            (0.6663102034, 2.002018839): {"roi1": 49},
+            (2.001407847, -2.002363463): {"roi1": 69},
+            (2.000183463, -0.9992309104): {"roi1": 97},
+            (1.999963432, 0.001258902382): {"roi1": 75},
+            (1.999884878, 1.001690624): {"roi1": 58},
+            (1.999828501, 2.001959397): {"roi1": 81},
+        }
+        assert np.array_equal(f["map"]["x"][:], [x[0] for x in should.keys()])
+        assert np.array_equal(f["map"]["y"][:], [x[1] for x in should.keys()])
+        assert np.array_equal(
+            f["map"]["data"]["roi1"], [y["roi1"] for y in should.values()]
+        )
+
         assert (
             f["version"]["commit_hash"][()]
             == b"1a474d1455f0d6c6113a22179b8fff1e98325916"
+        )
+        assert f["version"]["branch_name"][()] == b"main"
+        assert f["version"]["timestamp"][()] == b"2024-04-25T08:07:59+02:00"
+        assert (
+            f["version"]["repository_url"][()]
+            == b"https://gitlab.maxiv.lu.se/DanMAX/pipeline/drp-tomo-buffer"
         )
 
     loop = asyncio.get_event_loop()
