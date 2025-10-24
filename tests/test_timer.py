a2f5f20baff219ef591b1c740ca5644f7ef639bc
Michele Cascella <michele.cascella@maxiv.lu.se>
2025-10-24 14:07:19 +0200

    add timer test

---
+++ tests/test_timer.py
@@ -0,0 +1,127 @@
+import asyncio
+import logging
+from typing import Callable, Optional, Awaitable, Any, Coroutine
+
+import h5pyd
+import aiohttp
+import pytest
+import uvicorn
+import zmq
+from pydantic_core import Url
+
+from dranspose.controller import app
+from dranspose.ingester import Ingester
+from dranspose.ingesters.zmqpull_single import (
+    ZmqPullSingleIngester,
+    ZmqPullSingleSettings,
+)
+from dranspose.parameters import ParameterList
+from dranspose.protocol import (
+    WorkerName,
+    StreamName,
+)
+from dranspose.worker import Worker, WorkerSettings
+from tests.utils import wait_for_controller, wait_for_finish, set_uniform_sequence
+
+
+@pytest.mark.asyncio
+async def test_timer_params(
+    reducer: Callable[[Optional[str]], Awaitable[None]],
+    create_worker: Callable[[Worker], Awaitable[Worker]],
+    create_ingester: Callable[[Ingester], Awaitable[Ingester]],
+    stream_eiger: Callable[[zmq.Context[Any], int, int], Coroutine[Any, Any, None]],
+) -> None:
+    config = uvicorn.Config(app, port=5000, log_level="debug")
+    server = uvicorn.Server(config)
+    server_task = asyncio.create_task(server.serve())
+    while server.started is False:
+        await asyncio.sleep(0.1)
+
+    logging.info("started controller")
+    await asyncio.sleep(1)
+
+    await reducer("examples.params.reducer:ParamReducer")
+    await create_worker(
+        Worker(
+            settings=WorkerSettings(
+                worker_name=WorkerName("w1"),
+                worker_class="examples.params.worker:ParamWorker",
+            ),
+        )
+    )
+
+    await create_ingester(
+        ZmqPullSingleIngester(
+            settings=ZmqPullSingleSettings(
+                ingester_streams=[StreamName("eiger")],
+                upstream_url=Url("tcp://localhost:9999"),
+            ),
+        )
+    )
+
+    await asyncio.sleep(1)
+
+    state = await wait_for_controller(streams={StreamName("eiger")})
+    async with aiohttp.ClientSession() as session:
+        logging.warning("ensemble state uuids are %s", state.parameters_version)
+
+        assert state.parameters_hash is not None
+        assert state.reducer is not None
+        assert state.reducer.parameters_hash is not None
+        assert state.parameters_hash == state.reducer.parameters_hash
+        for ing in state.ingesters:
+            assert state.parameters_hash == ing.parameters_hash
+        for wo in state.workers:
+            assert state.parameters_hash == wo.parameters_hash
+
+        par = await session.get("http://localhost:5000/api/v1/parameters")
+        assert par.status == 200
+        params = ParameterList.validate_python(await par.json())
+
+        logging.warning("params %s", params)
+
+        ntrig = 10
+        await set_uniform_sequence({StreamName("eiger")}, ntrig)
+
+        context = zmq.asyncio.Context()
+
+        asyncio.create_task(stream_eiger(context, 9999, ntrig - 1))
+
+        await wait_for_finish()
+
+        def work() -> None:
+            f = h5pyd.File("http://localhost:5001/", "r")
+            logging.info(
+                f"file {list(f.keys())}",
+            )
+            logging.warning("version %s", list(f["params"].keys()))
+            assert f["params"]["roi1"][()] == b"bla"
+            assert f["worker_params"]["roi1"][()] == b"bla"
+
+        def work2() -> None:
+            f = h5pyd.File("http://localhost:5001/", "r")
+            logging.info(
+                f"file {list(f.keys())}",
+            )
+            logging.warning("version %s", list(f["params"].keys()))
+            assert f["params"]["roi1"][()] == b"new_value"
+            assert f["worker_params"]["roi1"][()] == b"bla"
+
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, work)
+
+        resp = await session.post(
+            "http://localhost:5000/api/v1/parameter/roi1",
+            data=b"new_value",
+        )
+        assert resp.status == 200
+
+        await asyncio.sleep(1.5)
+
+        await loop.run_in_executor(None, work2)
+
+        context.destroy()
+
+    server.should_exit = True
+    await server_task
+    await asyncio.sleep(0.1)
