7db6138f6d881c70c6015c6f275d28490b81a32f
Michele Cascella <michele.cascella@maxiv.lu.se>
2025-10-24 12:33:41 +0200

    fix publish_parameters

--- examples/params/reducer.py
+++ examples/params/reducer.py
@@ -9,14 +9,21 @@ class ParamReducer:
         self.number = 0
         self.publish = {"params": {}}
 
-    def process_result(self, result: ResultData, parameters=None):
+    def publish_parameters(self, parameters):
         logging.info("parameters are %s", parameters)
         self.publish["params"] = {n: p.value for n, p in parameters.items()}
-        self.publish["params"]["bytes_param"] = self.publish["params"][
-            "bytes_param"
-        ].decode()
-        self.publish["params"]["bool_param"] = int(self.publish["params"]["bool_param"])
+        if "bytes_param" in self.publish["params"]:
+            self.publish["params"]["bytes_param"] = self.publish["params"][
+                "bytes_param"
+            ].decode()
+        if "bool_param" in self.publish["params"]:
+            self.publish["params"]["bool_param"] = int(
+                self.publish["params"]["bool_param"]
+            )
         logging.info("params %s", self.publish["params"])
+
+    def process_result(self, result: ResultData, parameters=None):
+        self.publish_parameters(parameters)
         self.publish["worker_params"] = {n: p.value for n, p in result.payload.items()}
         self.publish["worker_params"]["bytes_param"] = self.publish["worker_params"][
             "bytes_param"
@@ -26,8 +33,8 @@ class ParamReducer:
         )
 
     def timer(self, parameters=None):
-        logging.info("parameters are %s", parameters)
-        self.publish["params"] = parameters
+        self.publish_parameters(parameters)
+        return 1
 
     def finish(self, parameters=None):
         logging.info("finished dummy reducer work")
