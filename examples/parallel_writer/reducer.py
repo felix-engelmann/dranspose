771e5948d2441128164afb1229bec1ed951187cb
Michele Cascella <michele.cascella@maxiv.lu.se>
2026-02-27 14:40:34 +0100

    add some info to publish

--- examples/parallel_writer/reducer.py
+++ examples/parallel_writer/reducer.py
@@ -1,13 +1,23 @@
-import logging
 from collections import defaultdict
+import logging
 
 from dranspose.event import ResultData
 from dranspose.protocol import ReducerState
 
+logger = logging.getLogger(__name__)
+
 
 class WriterReducer:
     def __init__(self, state: ReducerState | None = None, **kwargs: dict) -> None:
-        self.publish: dict[str, dict] = {"workers": {}, "frames": defaultdict(list)}
+        self.workers = {}
+        self.frames = defaultdict(list)
+        self.filename = ""
+        self.publish: dict[str, dict] = {
+            "workers": self.workers,
+            "frames": self.frames,
+            "filename": self.filename,
+        }
+        self.name = state.name
 
     def process_result(
         self, result: ResultData, parameters: dict | None = None
@@ -16,12 +26,13 @@ class WriterReducer:
         wname = result.payload["worker_name"]
         logging.info("result is %s", result.payload)
         if "header" in result.payload:
-            self.publish["workers"][wname] = result.payload["header"]
+            self.workers[wname] = result.payload["header"]
+            self.filename = result.payload["header"]["master_filename"]
         elif "frame" in result.payload:
             info = result.payload["frame"]
             logging.info("frame info are %s", info)
             res = (info["frame_number"], info["position"])
-            self.publish["frames"][wname].append(res)
+            self.frames[wname].append(res)
 
     def finish(self, parameters: dict | None = None) -> None:
-        print("finished dummy reducer work")
+        pass
